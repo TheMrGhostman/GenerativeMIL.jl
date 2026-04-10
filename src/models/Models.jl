bcbb0e3f7abf4c92e3e93cac6b8126f0964df1d2
TheMrGhostman <ghostmanthe@gmail.com>
2026-04-10 17:06:03 +0200

    model exports

--- src/models/Models.jl
+++ src/models/Models.jl
@@ -2,18 +2,31 @@
 
 
 include("SetVAE.jl")
-export SetVae, loss, loss_gpu, setvae_constructor_from_named_tuple
+export SetVae, loss, loss_gpu
+export setvae_constructor_from_named_tuple
 
 include("FoldingVAE.jl")
+export FoldingNet_VAE, simple_loss, logging_loss
+export foldingnet_constructor_from_named_tuple
 
 include("PoolAE.jl")
+export PoolModel, loss, loss_with_kld
+export poolmodel_constructor_from_named_tuple
 
 include("SetTransformer.jl")
+export SetClassifier, loss
 
 include("SetVAEformer.jl") # TODO finish this
 
 include("vae.jl")
+export VariationalAutoencoder, loss
 
 include("VQVAE.jl")
+export VectorQuantizer, VectorQuantizerEMA
+export VQVAE, loss_ema, loss_gradient
+export vqvae_constructor_from_named_tuple
 
 include("VQVAE_PoolAE.jl")
+export VectorGaussianQuantizerEMA, ema_update!
+export VQ_PoolAE, VGQ_PoolAE, loss_gradient, loss_ema
+export vgq_poolae_constructor_from_named_tuple, vgq_poolae_constructor_from_named_tuple
