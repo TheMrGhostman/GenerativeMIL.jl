0a162ff3b6550edb78f84a95fce14c2b76725e4a
TheMrGhostman <ghostmanthe@gmail.com>
2026-06-08 13:16:07 +0200

    update

--- scripts/process_shapenet.jl
+++ scripts/process_shapenet.jl
@@ -147,9 +147,6 @@ function plot_pointcloud_sample(x)
 end
 
 
-
-pth = "/mnt/personal/zorekmat/Datasets/ShapeNetCore/"
-
 function process_airplanes(npoints=8192; normalize::Bool=true)
     airplane_id = "02691156"
     airplane_folders = readdir(joinpath(pth, airplane_id), join=true)
@@ -173,3 +170,223 @@ function process_airplanes(npoints=8192; normalize::Bool=true)
     return res_dict
 end
 #serialize("/home/zorekmat/MIL/GenerativeMIL/data/datasets/shapenetcore/airplanes_normalized_$(npoints).jls", res_dict)
+
+
+function process_shapenet_folder(path, folder_id, classname=nothing; npoints=8192, normalize::Bool=true)
+    subfolders = readdir(joinpath(path, folder_id), join=true)
+    objects = map(x->joinpath(x, "models", "model_normalized.obj"), subfolders)
+    objects = filter(ispath, objects)
+    @info "found $(length(subfolders) - length(objects)) invalid files or files without `.obj` files"
+    #airplane_folders[1]|>readdir
+    #test
+    #a = sample_pointcloud_from_obj(airplanes[4], 8192);
+    #plot_pointcloud_sample(a)
+
+    PClouds = zeros(Float32, 3, npoints, length(objects))
+    for i in tqdm(axes(objects,1))
+        PClouds[:,:,i] .= sample_pointcloud_from_obj(objects[i], npoints; normalize=normalize)
+    end
+
+    res_dict = Dict(
+            "features" => PClouds,
+            "targets" => zeros(length(objects)),
+            "classes" => fill((classname === nothing) ? folder_id : classname, length(objects)),
+            "folder_id" => folder_id
+        )
+    return res_dict
+end
+
+function load_class_from_15K_shapenet(class_id)
+    tr_files = readdir("/mnt/personal/zorekmat/Datasets/ShapeNetCore.v2.PC15k/$(class_id)/train", join=true); 
+    val_files = readdir("/mnt/personal/zorekmat/Datasets/ShapeNetCore.v2.PC15k/$(class_id)/val", join=true); 
+    tst_files = readdir("/mnt/personal/zorekmat/Datasets/ShapeNetCore.v2.PC15k/$(class_id)/test", join=true); 
+
+    @info "loading train split"
+    x_train = zeros(Float32, 3, 15000, length(tr_files))
+    for (i, file) in enumerate(tqdm(tr_files))
+        x = npzread(file)
+        x_train[:,:,i] .= x'
+    end
+
+    @info "loading train split"
+    x_val = zeros(Float32, 3, 15000, length(val_files))
+    for (i, file) in enumerate(tqdm(val_files))
+        x = npzread(file)
+        x_val[:,:,i] .= x'
+    end
+
+    @info "loading train split"
+    x_test = zeros(Float32, 3, 15000, length(tst_files))
+    for (i, file) in enumerate(tqdm(tst_files))
+        x = npzread(file)
+        x_test[:,:,i] .= x'
+    end
+
+    return (train = x_train, valid = x_val, test = x_test)
+end
+
+
+function load_and_save_15K(classes)
+    for (class_id, class_name) in classes
+        out = load_class_from_15K_shapenet(class_id)
+        serialize("/home/zorekmat/MIL/GenerativeMIL/data/datasets/shapenetcore/$(class_name)_15000.jls", out)
+    end
+end
+
+"""
+load_and_save_15K(
+    [
+        ("02691156", "airplane"),
+        ("02958343", "car"),
+        ("04379243", "table"),
+        ("03001627", "chair"),
+        ("04256520", "sofa"),
+        ("04090263", "rifle"),
+        ("03636649", "lamp"),
+        ("04530566", "watercraft"),
+        ("03691459", "loudspeaker"),
+        ("03211117", "display")
+    ]
+)
+"""
+
+
+
+
+
+pth = "/mnt/personal/zorekmat/Datasets/ShapeNetCore/"
+
+
+res = process_shapenet_folder(pth, "02691156", "airplane"; npoints=8192)
+#serialize("/home/zorekmat/MIL/GenerativeMIL/data/datasets/shapenetcore/airplane_normalized_8192.jls", res)
+
+res = process_shapenet_folder(pth, "02691156", "airplane"; npoints=8192, normalize=false)
+#serialize("/home/zorekmat/MIL/GenerativeMIL/data/datasets/shapenetcore/airplane_8192.jls", res)
+
+res = process_shapenet_folder(pth, "02958343", "car"; npoints=8192)
+#serialize("/home/zorekmat/MIL/GenerativeMIL/data/datasets/shapenetcore/car_normalized_8192.jls", res)
+
+res = process_shapenet_folder(pth, "02958343", "car"; npoints=8192, normalize=false)
+#serialize("/home/zorekmat/MIL/GenerativeMIL/data/datasets/shapenetcore/car_8192.jls", res)
+
+res = process_shapenet_folder(pth, "04379243", "table"; npoints=8192)
+#serialize("/home/zorekmat/MIL/GenerativeMIL/data/datasets/shapenetcore/table_normalized_8192.jls", res)
+
+res = process_shapenet_folder(pth, "04379243", "table"; npoints=8192, normalize=false)
+#serialize("/home/zorekmat/MIL/GenerativeMIL/data/datasets/shapenetcore/table_8192.jls", res)
+
+res = process_shapenet_folder(pth, "03001627", "chair"; npoints=8192, normalize=true)
+#serialize("/home/zorekmat/MIL/GenerativeMIL/data/datasets/shapenetcore/chair_normalized_8192.jls", res)
+
+res = process_shapenet_folder(pth, "03001627", "chair"; npoints=8192, normalize=false)
+#serialize("/home/zorekmat/MIL/GenerativeMIL/data/datasets/shapenetcore/chair_8192.jls", res)
+
+
+res = process_shapenet_folder(pth, "04256520", "sofa"; npoints=8192, normalize=true)
+serialize("/home/zorekmat/MIL/GenerativeMIL/data/datasets/shapenetcore/sofa_normalized_8192.jls", res)
+
+res = process_shapenet_folder(pth, "04256520", "sofa"; npoints=8192, normalize=false)
+serialize("/home/zorekmat/MIL/GenerativeMIL/data/datasets/shapenetcore/sofa_8192.jls", res)
+
+
+
+
+using NPZ
+
+x = npzread("/mnt/personal/zorekmat/Datasets/ShapeNetCore.v2.PC15k/02691156/train/816935cac027310d5e9e2656aff7dd5b.npy")  # typicky 15000 × 3
+
+println(size(x))
+println(eltype(x))
+
+println("min per axis: ", minimum(x, dims=1))
+println("max per axis: ", maximum(x, dims=1))
+println("mean per axis: ", mean(x, dims=1))
+
+center = mean(x, dims=1)
+radii = sqrt.(sum((x .- center).^2, dims=2))
+println("max radius: ", maximum(radii))
+println("mean radius: ", mean(radii))
+
+
+plot_pointcloud_sample(permutedims(x, (2,1)))
+
+files = readdir("/mnt/personal/zorekmat/Datasets/ShapeNetCore.v2.PC15k/02691156/train", join=true); 
+
+μ_s = zeros(Float32, 3, length(files));
+Σ_s = zeros(Float32, length(files));
+
+for i in tqdm(axes(files,1))
+    x = npzread(files[i]);
+    center = mean(x, dims=1)
+    radii = sqrt.(sum((x .- center).^2, dims=2))
+    μ_s[:,i] .= center[1,:]
+    Σ_s[i] = maximum(radii)
+end
+
+mean(μ_s, dims=2)
+median(μ_s, dims=2)
+
+mean(Σ_s)
+median(Σ_s)
+maximum(Σ_s)
+
+files = readdir("/mnt/personal/zorekmat/Datasets/ShapeNetCore.v2.PC15k/02958343/train", join=true); 
+
+μ_s = zeros(Float32, 3, length(files));
+Σ_s = zeros(Float32, length(files));
+
+for i in tqdm(axes(files,1))
+    x = npzread(files[i]);
+    center = mean(x, dims=1)
+    radii = sqrt.(sum((x .- center).^2, dims=2))
+    μ_s[:,i] .= center[1,:]
+    Σ_s[i] = maximum(radii)
+end
+
+mean(μ_s, dims=2)
+median(μ_s, dims=2)
+
+mean(Σ_s)
+median(Σ_s)
+maximum(Σ_s)
+
+
+
+files = readdir("/mnt/personal/zorekmat/Datasets/ShapeNetCore.v2.PC15k/03001627/train", join=true); 
+
+μ_s = zeros(Float32, 3, length(files));
+Σ_s = zeros(Float32, length(files));
+
+for i in tqdm(axes(files,1))
+    x = npzread(files[i]);
+    center = mean(x, dims=1)
+    radii = sqrt.(sum((x .- center).^2, dims=2))
+    μ_s[:,i] .= center[1,:]
+    Σ_s[i] = maximum(radii)
+end
+
+mean(μ_s, dims=2)
+median(μ_s, dims=2)
+
+mean(Σ_s)
+median(Σ_s)
+maximum(Σ_s)
+
+
+μ_s = zeros(Float32, 3, length(v));
+Σ_s = zeros(Float32, length(v));
+
+for i in tqdm(axes(v,1))
+    x = v[i]
+    center = mean(x, dims=2)
+    radii = sqrt.(sum((x .- center).^2, dims=1))
+    μ_s[:,i] .= center[:,1]
+    Σ_s[i] = maximum(radii)
+end
+
+mean(μ_s, dims=2)
+median(μ_s, dims=2)
+
+mean(Σ_s)
+median(Σ_s)
+maximum(Σ_s)
